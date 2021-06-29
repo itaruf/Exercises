6f08c84eb0870e14ed76b1752712be37dad14a7f
itaruf <imane93100@hotmail.fr>
2021-06-29 04:15:35 +0200

    Updating

---
+++ Unity Projects/FruitNinja/Assets/Scripts/Bomb.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Blade b = collision.GetComponent<Blade>();
+        if (!b)
+            return;
+        else
+        {
+            FindObjectOfType<GameManager>().OnBombHit();
+        }
+    }
+}
---
+++ Unity Projects/FruitNinja/Assets/Scripts/GameManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    [Header("Score Elements")]
+    public int score;
+    public int highscore;
+    public Text scoreText;
+    public Text highscoreText;
+
+
+    [Header("GameOver")]
+    public GameObject gameOverPanel;
+    public Text gameOverPanelScoreText;
+    public Text gameOverPanelHighScoreText;
+
+    [Header("Sounds")]
+    public AudioClip[] sliceSounds;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+
+        audioSource = GetComponent<AudioSource>();
+        gameOverPanel.SetActive(false);
+        GetHighscore();
+    }
+
+    private void GetHighscore(){
+
+		highscore = PlayerPrefs.GetInt("Highscore");
+		highscoreText.text = "Best: " + highscore;
+    }
+
+
+    public void IncreaseScore(int points){
+        score += points;
+        scoreText.text = score.ToString();
+
+        if(score > highscore){
+            PlayerPrefs.SetInt("Highscore", score);
+            highscoreText.text = score.ToString();
+        }
+
+    }
+
+    public void OnBombHit(){
+        Time.timeScale = 0;
+
+        gameOverPanelScoreText.text = "Score: " + score.ToString();
+        gameOverPanelHighScoreText.text = "Best: " + highscore.ToString();
+        gameOverPanel.SetActive(true);
+
+        Debug.Log("Bomb hit");
+    }
+
+    public void RestartGame(){
+
+        score = 0;
+        scoreText.text = score.ToString();
+
+        gameOverPanel.SetActive(false);
+
+        /*foreach (GameObject g in GameObject.FindGameObjectsWithTag("Interactable")){
+            Destroy(g);
+        }*/
+
+        Time.timeScale = 1;
+    }
+
+    public void PlayRandomSliceSound(){
+        AudioClip randomSound = sliceSounds[Random.Range(0, sliceSounds.Length)];
+        audioSource.PlayOneShot(randomSound);
+    }
+
+}
