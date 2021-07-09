2ebb81fdd9f7f0ae0c052bd106a3789dff10af30
itaruf <imane93100@hotmail.fr>
2021-07-09 02:50:20 +0200

    Updating

---
+++ HTML Projects/imane-t.com/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.overlay').css('visibility','visible').hide().fadeIn(2000);
+    $('body').scrollspy({ target: '#navbar-example2' })
+
+/*     setInterval(function() {
+                $("#about-title").find("h1").fadeTo(2000, 0.33).fadeTo(2000, 1);
+                $("#about-title").find("hr").fadeTo(2000, 0.33).fadeTo(2000, 1);
+                 $("#unreal-title").find("h1").fadeTo(2000, 0.33).fadeTo(2000, 1);
+                 $("#unreal-title").find("hr").fadeTo(2000, 0.33).fadeTo(2000, 1);
+                 $("#unity-title").find("h1").fadeTo(2000, 0.33).fadeTo(2000, 1);
+                  $("#unity-title").find("hr").fadeTo(2000, 0.33).fadeTo(2000, 1);
+                 $("#pixelart-title").find("h1").fadeTo(2000, 0.33).fadeTo(2000, 1);
+       }, 2000); */
+});
