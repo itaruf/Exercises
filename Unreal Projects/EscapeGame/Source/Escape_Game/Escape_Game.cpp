1f642344ff141ec39a1b1263cb2d4edc2c0533fd
itaruf <imane93100@hotmail.fr>
2022-12-21 17:41:25 +0100

    UE / Unity Projects Removed

--- Unreal Projects/EscapeGame/Source/Escape_Game/Escape_Game.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Escape_Game.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Escape_Game, "Escape_Game" );
